465731560ec4eec47a504b30bf06208d02d003c8
ramil27 <gahramanov_ramil@icloud.com>
2021-10-23 19:30:46 +0600

    ll

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from category.models import Category
 
-# Create your models here.
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='image', upload_to='product')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    ip = models.CharField(max_length=50, verbose_name='ip')
+
+    def str(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'slug': self.slug})
